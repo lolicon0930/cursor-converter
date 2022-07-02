ed1fb2c144d2c4dcca736ecca38e3e7446b51b3b
lolicon0930 <14966910+lolicon0930@users.noreply.github.com>
2022-07-03 07:39:10 +0800

    20220703
    Make some modifications that are convenient for my own use.
    
    Signed-off-by: lolicon0930 <14966910+lolicon0930@users.noreply.github.com>

--- ani2ico/ani2ico.c
+++ ani2ico/ani2ico.c
@@ -10,10 +10,10 @@ int test_for_icon_string(char *buffer,int start);
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
+	if (argc != 2) {
 		fprintf(stderr, "Please give an .ani filename as an argument.\n");
-        return 0;
-    }
+		return 0;
+	}
 
 	ReadFile(*(argv+1));
 	return 0;
@@ -21,15 +21,15 @@ int main(int argc, char **argv)
 
 int test_for_icon_string(char *buffer,int start)
 {
-    if (*(buffer+start) == 0x69) {
-        if (*(buffer+start+1) == 0x63) {
-            if (*(buffer+start+2) == 0x6f) {
-                if (*(buffer+start+3) == 0x6e) {
-                    return 1;
-                }
-            }
-        }
-    } else return 0;
+	if (*(buffer+start) == 0x69) {
+		if (*(buffer+start+1) == 0x63) {
+			if (*(buffer+start+2) == 0x6f) {
+				if (*(buffer+start+3) == 0x6e) {
+					return 1;
+				}
+			}
+		}
+	} else return 0;
 }
 
 void ReadFile(char *name)
@@ -38,27 +38,27 @@ void ReadFile(char *name)
 	char *buffer,*fileName;
 	unsigned long fileLen;
 
-    if (!strstr(name,".ani")) {
+	if (!strstr(name,".ani")) {
 		fprintf(stderr, "Please give an .ani filename as an argument.\n");
-        return;
-    }
+		return;
+	}
 
-    fileName=malloc(strlen(name)+1);
-    if (!fileName) {
+	fileName=malloc(strlen(name)+1);
+	if (!fileName) {
 		fprintf(stderr, "Memory error!\n");
-        return;
-    }
+		return;
+	}
 
-    strcpy(fileName,name);
-    *strstr(fileName,".ani")='\0';
+	strcpy(fileName,name);
+	*strstr(fileName,".ani")='\0';
 
 	file = fopen(name, "rb");
 	if (!file) {
 		fprintf(stderr, "Unable to open file %s.\n", name);
-        free(fileName);
+		free(fileName);
 		return;
 	}
-	
+
 	fseek(file, 0, SEEK_END);
 	fileLen=ftell(file);
 	fseek(file, 0, SEEK_SET);
@@ -66,72 +66,72 @@ void ReadFile(char *name)
 	buffer=(char *)malloc(fileLen+1);
 	if (!buffer) {
 		fprintf(stderr, "Memory error!\n");
-        fclose(file);
-        free(fileName);
+		fclose(file);
+		free(fileName);
 		return;
 	}
 
 	fread(buffer, fileLen, 1, file);
 	fclose(file);
 
-    char *new_ico_name=malloc(strlen(name)+5);
-    if (!new_ico_name) {
+	char *new_ico_name=malloc(strlen(name)+5);
+	if (!new_ico_name) {
 		fprintf(stderr, "Memory error!\n");
-        free(fileName);
-        free(buffer);
-        return;
-    }
+		free(fileName);
+		free(buffer);
+		return;
+	}
 
-    char ico_counter_string[5];
+	char ico_counter_string[5];
 	int i,j,ico_counter=1;
-    FILE *ico_image;
+	FILE *ico_image;
 
 	for (i=0;i <= fileLen;i++) {
-        if (ico_counter == 9999) {
+		if (ico_counter == 9999) {
 			free(fileName);
 			free(buffer);
 			free(new_ico_name);
-            return;
+			return;
 		}
-		
-        if ((i+4 <= fileLen) && test_for_icon_string(buffer,i) == 1) {
-            sprintf(ico_counter_string,"%d",ico_counter);
-            strcpy(new_ico_name,fileName);
-            strcat(new_ico_name,ico_counter_string);
-            strcat(new_ico_name,".ico");
-            ico_counter++;
 
-            ico_image = fopen(new_ico_name,"wb");
-            if (!ico_image) {
-                fprintf(stderr, "Unable to open file %s.\n", new_ico_name);
-                free(fileName);
-                free(buffer);
-                free(new_ico_name);
-                return;
-            }
+		if ((i+4 <= fileLen) && test_for_icon_string(buffer,i) == 1) {
+			sprintf(ico_counter_string,"%04d",ico_counter);
+			strcpy(new_ico_name,fileName);
+			strcat(new_ico_name,ico_counter_string);
+			strcat(new_ico_name,".ico");
+			ico_counter++;
 
-            j=8;
-            while (i+j+4 <= fileLen) {
-                if (test_for_icon_string(buffer,i+j+1) == 1)
-                    break;
-                if (j==10)
-                    putc(0x01,ico_image);
-                else
-                    putc(*(buffer+i+j),ico_image);
-                j++;
-            }
-            if (i+j <= fileLen)
-                putc(*(buffer+i+j),ico_image);
-            if (fileLen-i-j<=3) {
-                putc(*(buffer+i+j+1),ico_image);
-                putc(*(buffer+i+j+2),ico_image);
-            }
-            fclose(ico_image);
-            i+=j;
-        }
-    }
-        
-    free(new_ico_name);
-    free(buffer);
-    free(fileName);
+			ico_image = fopen(new_ico_name,"wb");
+			if (!ico_image) {
+				fprintf(stderr, "Unable to open file %s.\n", new_ico_name);
+				free(fileName);
+				free(buffer);
+				free(new_ico_name);
+				return;
+			}
+
+			j=8;
+			while (i+j+4 <= fileLen) {
+				if (test_for_icon_string(buffer,i+j+1) == 1)
+					break;
+				if (j==10)
+					putc(0x01,ico_image);
+				else
+					putc(*(buffer+i+j),ico_image);
+				j++;
+			}
+			if (i+j <= fileLen)
+				putc(*(buffer+i+j),ico_image);
+			if (fileLen-i-j<=3) {
+				putc(*(buffer+i+j+1),ico_image);
+				putc(*(buffer+i+j+2),ico_image);
+			}
+			fclose(ico_image);
+			i+=j;
+		}
+	}
+
+	free(new_ico_name);
+	free(buffer);
+	free(fileName);
 }
--- readme.md
+++ readme.md
@@ -1,28 +1,17 @@
 # Windows to Linux cursor converter
 
-![dinosaur](screenshots/dinosau21.png)
-![dinosaur](screenshots/dinosau22.png)
-![dinosaur](screenshots/dinosau23.png)
-![dinosaur](screenshots/dinosau24.png)
-![dinosaur](screenshots/dinosau25.png)
-![dinosaur](screenshots/dinosau26.png)
-![dinosaur](screenshots/dinosaur1.png)
-![dinosaur](screenshots/dinosaur2.png)
-![dinosaur](screenshots/dinosaur3.png)
-![dinosaur](screenshots/dinosaur4.png)
-![dinosaur](screenshots/dinosaur5.png)
-![dinosaur](screenshots/dinosaur6.png)
-
 ## Depends
 
 - ImageMagick
-- Windows Cursors, either `.ani` or `.cur`. (A default dinosaur theme is included)
+- Windows Cursors, either `.ani` or `.cur`.
 
-## Build Ani2Ico
+## If you want to build ani2ico yourself
 
 ```
 cd ani2ico
+rm ani2ico
 make
+strip ani2ico
 ```
 
 ## Conversion
