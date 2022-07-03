bea4cc09c4996c4778c2619d1f06db0b7d3d530e
lolicon0930 <14966910+lolicon0930@users.noreply.github.com>
2022-07-03 22:14:26 +0800

    Handling filenames containing spaces.
    Signed-off-by: lolicon0930 <14966910+lolicon0930@users.noreply.github.com>

--- convert.sh
+++ convert.sh
@@ -5,33 +5,37 @@ cd outputs
 
 if compgen -G "../inputs/*.ani" > /dev/null; then
 	for name in ../inputs/*.ani; do
-		name=$(basename $name .ani)
-		mkdir -p $name
-		cd $name
-		cp ../../inputs/$name.ani .
-		../../ani2ico/ani2ico $name.ani
-		rm $name.ani
-		for f in $name*.ico; do
-			filename=$(basename "$f")
-			png="${filename%.*}".png
+		name=$(basename "$name" .ani)
+		new_name=${name//[[:space:]]/_}
+		mkdir -p $new_name
+		cd $new_name
+		cp ../../inputs/"$name".ani ./$new_name.ani
+		../../ani2ico/ani2ico $new_name.ani
+		rm $new_name.ani
+		for f in *.ico; do
+			filename=$(basename $f)
+			png=${filename%.*}.png
 			convert $f $png
-			identify -format '%w 1 1 %f 200\n' $png >> $name.xcg
+			identify -format '%w 1 1 %f 200\n' $png >> $new_name.xcg
 		done
-		xcursorgen $name.xcg $name
+		xcursorgen $new_name.xcg $new_name
 		cd ..
+		echo $new_name".ani convert complete."
 	done
 fi
 
 if compgen -G "../inputs/*.cur" > /dev/null; then
 	for name in ../inputs/*.cur; do
-		name=$(basename $name .cur)
-		mkdir -p $name
-		cd $name
-		cp ../../inputs/$name.cur .
-		convert $name.cur $name.png
-		identify -format '%w 1 1 %f\n' $name.png >> $name.xcg
-		xcursorgen $name.xcg $name
+		name=$(basename "$name" .cur)
+		new_name=${name//[[:space:]]/_}
+		mkdir -p $new_name
+		cd $new_name
+		cp ../../inputs/"$name".cur ./$new_name.cur
+		convert $new_name.cur $new_name.png
+		identify -format '%w 1 1 %f\n' $new_name.png >> $new_name.xcg
+		xcursorgen $new_name.xcg $new_name
 		cd ..
+		echo $new_name".cur convert complete."
 	done
 fi
 
